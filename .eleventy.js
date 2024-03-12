f72b274769baa5baaa5fe95fcaa1cfb27c84cb4c
brooklando <144083577+brooklando@users.noreply.github.com>
2024-03-12 02:56:35 +0000

    testing tailwind centering

--- .eleventy.js
+++
@@ -1,8 +0,0 @@
-function randomName() {
-  const names = ['Max', 'Malcolm', 'Mason', 'Macy', 'Marissa', 'Maurice', 'Mike', 'Madison', 'Melanie', 'Melon'];
-  return {
-    get name() {
-      return names[Math.floor(Math.random() * names.length)];
-    },
-  }
-}
